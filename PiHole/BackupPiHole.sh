209a19eaa1b0308d6858032e94f668f36019cf64
A.J. Orians <ajorians@gmail.com>
2023-06-11 08:32:22 -0400

    Remove cookie header.

--- PiHole/BackupPiHole.sh
+++ PiHole/BackupPiHole.sh
@@ -29,7 +29,6 @@ output=`curl "$piholeurl/admin/settings.php" \
   -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
   -H 'Accept-Language: en-US,en;q=0.9' \
   -H 'Connection: keep-alive' \
-  -H 'Cookie: PHPSESSID=8uin150gc7c0nci53qhatck6ch' \
   -H "Referer: $piholeurl/admin/settings.php?tab=sysadmin" \
   -H 'Upgrade-Insecure-Requests: 1' \
   -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36' \
