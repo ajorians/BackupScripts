b743d3702a5f58f2597673090909a144517fbac3
A.J. Orians <ajorians@gmail.com>
2023-09-10 21:49:48 -0400

    Started backup script for MediaWiki.

---
+++ MediaWiki/BackupMediaWiki.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+databasepass=`cat /root/passwords/databasepass.txt`
+
+mkdir /mnt/backups
+mkdir /mnt/backups/mediawiki
+
+DATE=`date +%F`
+FILENAME=mediawiki-$DATE.tar.gz
+SRCDIR=/srv/www/htdocs/mediawiki
+DESTDIR=/mnt/backups/mediawiki
+tar --gzip -cf "$DESTDIR/$FILENAME" $SRCDIR
+
+FILENAMEDB=mediawikidb-$DATE.tar.gz
+
+mysqldump -u root -p $databasepass mediawiki > "$DESTDIR/$FILENAMEDB"
+
+## Delete older backups
+find "$DESTDIR/$FILENAME" -type f -mtime +10 -delete
+find "$DESTDIR/$FILENAMEDB" -type f -mtime +10 -delete
