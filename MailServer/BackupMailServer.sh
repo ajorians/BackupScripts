ae19b880ba749988d077073378e9a6423d8658b0
A.J. Orians <ajorians@gmail.com>
2023-09-14 22:51:27 -0400

    Added script for Mailserver Docker data.

---
+++ MailServer/BackupMailServer.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+
+databasepass=`cat /root/passwords/databasepass.txt`
+
+mkdir /mnt/backups
+mkdir /mnt/backups/mailserver
+
+DATE=`date +%F`
+FILENAME=mailserver-$DATE.tar.gz
+SRCDIR=/opt/DockerSwarmData
+DESTDIR=/mnt/backups/mailserver
+tar --gzip -cf "$DESTDIR/$FILENAME" -C $SRCDIR mailserver
+
+## Delete older backups
+find "$DESTDIR/$FILENAME" -type f -mtime +10 -delete
