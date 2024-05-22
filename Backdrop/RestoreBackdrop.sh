63cf873a096721f68d6aa912d5b995457931015f
A.J. Orians <ajorians@raspberry400.local>
2024-05-22 07:46:22 -0400

    Updated Restore Backdrop.

--- Backdrop/RestoreBackdrop.sh
+++ Backdrop/RestoreBackdrop.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
 
-databasepass=`cat /root/passwords/databasepass.txt`
-
-echo "Enter full path to Backdrop files backup:"
-read backdropbackup
-
-echo "Enter full path to Backdrop database backup:"
-read backdropdbbackup
-
-if [ ! -f "$backdropbackup" ]; then
-    echo "$backdropbackup does not exist."
+if [ ! -f /root/passwords/databasepass.txt ]; then
+    echo "Database file not found!"
     exit 1
 fi
 
-if [ ! -f "$backdropdbbackup" ]; then
-    echo "$backdropdbbackup does not exist."
+databasepass=`cat /root/passwords/databasepass.txt`
+
+ask()
+{
+  declare -g $1="$2"
+  if [ -z "${!1}" ]; then
+    echo "$3"
+    read $1
+  fi
+}
+
+ask BACKDROPBACKUP "$1" "Full path to Backdrop backup .tar.gz file:"
+ask BACKDROPDBBACKUP "$2" "Full path to Backdrop database backup .sql file:"
+
+if [ ! -f "$BACKDROPBACKUP" ]; then
+    echo "$BACKDROPBACKUP does not exist."
+    exit 1
+fi
+
+if [ ! -f "$BACKDROPDBBACKUP" ]; then
+    echo "$BACKDROPDBBACKUP does not exist."
     exit 1
 fi
 
 mysql -u root -p$databasepass -e "create database backdrop"; 
-mysql -u root -p$databasepass backdrop < $backdropdbbackup
+mysql -u root -p$databasepass backdrop < $BACKDROPDBBACKUP
 
-tar -xvf $backdropbackup -C /srv/www/htdocs
+tar -xvf $BACKDROPBACKUP -C /srv/www/htdocs
 
