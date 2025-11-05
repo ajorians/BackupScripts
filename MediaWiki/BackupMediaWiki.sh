896283e71dd818014cfad38934a8c7c3ed146522
A.J. Orians <ajorians@gmail.com>
2025-11-04 21:37:11 -0500

    Only backup database since using Docker images.

--- MediaWiki/BackupMediaWiki.sh
+++ MediaWiki/BackupMediaWiki.sh
@@ -6,15 +6,11 @@ mkdir /mnt/sync/backups
 mkdir /mnt/sync/backups/mediawiki
 
 DATE=`date +%F`
-FILENAME=mediawiki-$DATE.tar.gz
-SRCDIR=/srv/www/htdocs
 DESTDIR=/mnt/sync/backups/mediawiki
-tar --gzip -cf "$DESTDIR/$FILENAME" -C $SRCDIR mediawiki-1.44.0
 
 FILENAMEDB=mediawikidb-$DATE.sql
 
 mysqldump -u root -p$databasepass mediawiki > "$DESTDIR/$FILENAMEDB"
 
 ## Delete older backups
-ls -t $DESTDIR/mediawiki-*.tar.gz | tail -n +10 | xargs rm --
 ls -t $DESTDIR/mediawikidb-*.sql | tail -n +10 | xargs rm --
